a5e13b5eacd2aef97f029f056225730461d3a9ec
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-04-19 20:07:06 +0300

    Testmod!

--- src/testmod/java/io/github/speedbridgemc/entityevents/testmod/Initializer.java
+++ src/testmod/java/io/github/speedbridgemc/entityevents/testmod/Initializer.java
@@ -1,6 +1,8 @@
 package io.github.speedbridgemc.entityevents.testmod;
 
+import io.github.speedbridgemc.entityevents.api.EntityDamageEvents;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.EntityType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,5 +12,12 @@ public final class Initializer implements ModInitializer {
     @Override
     public void onInitialize() {
         LOGGER.info("Hello from the testmod!");
+        // attacks which don't kill Creepers ignite them
+        EntityDamageEvents.of(EntityType.CREEPER).registerAfter((entity, source, amount) -> {
+            if (!entity.world.isClient) {
+                if (entity.getHealth() - amount > 0)
+                    entity.ignite();
+            }
+        });
     }
 }
